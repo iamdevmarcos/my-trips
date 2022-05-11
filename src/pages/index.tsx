5242fd5d4f0acea5930495e34918010bd918bb7f
Marcos Andre <marcosdev.me@gmail.com>
2022-05-11 20:11:23 -0300

    feat: test driven development

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,5 +3,29 @@ import dynamic from 'next/dynamic'
 const Map = dynamic(() => import('components/Map'), { ssr: false })
 
 export default function Home() {
-  return <Map />
+  // if ('navigator' in window) {
+  //   const myLocation = navigator.geolocation.getCurrentPosition((position) =>
+  //     console.log(`
+  //       LATITUDE: ${position.coords.latitude}
+  //       LONGITUDE: ${position.coords.longitude}
+  //     `)
+  //   )
+  //   console.log(myLocation)
+  // }
+
+  return (
+    <Map
+      places={[
+        {
+          id: '2',
+          name: 'Reykjavik',
+          slug: 'reykjavik',
+          location: {
+            latitude: -3.662902153852959,
+            longitude: -38.67868396259939
+          }
+        }
+      ]}
+    />
+  )
 }
