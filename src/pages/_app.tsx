a14976a62606f51ed01971c5010d488ecce55be0
Marcos Andre <marcosdev.me@gmail.com>
2022-05-11 18:42:22 -0300

    feat: adding map component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,12 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
+          integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=="
+          crossOrigin=""
+        />
         <meta
           name="description"
           content="A simple project to show my favorite spots in the world"
