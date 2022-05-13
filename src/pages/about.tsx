e55892e4297e7c129fb7c6b6e9b93142fa03e52f
Marcos Andre <marcosdev.me@gmail.com>
2022-05-13 06:52:49 -0300

    feat: requesting graphql server

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,23 @@
+import client from 'graphql/client'
+import { GET_PAGES } from 'graphql/querys'
+
 import AboutTemplate from 'templates/About'
 
-const About = () => <AboutTemplate />
+export default function AboutPage() {
+  return <AboutTemplate />
+}
 
-export default About
+export const getStaticProps = async () => {
+  const { pages } = await client.request(GET_PAGES)
+
+  console.log(pages)
+
+  return {
+    props: {}
+  }
+}
+
+// getStaticPaths => serve para gerar as urls em build time: /about
+// getStaticProps => serve para buscar dados da pagina (props) - build time
+// getServerSideProps => serve para buscar dados da pagina (props) - runtime
+// getInitialProps => serve para buscar dados da pagina (props) - runtime
