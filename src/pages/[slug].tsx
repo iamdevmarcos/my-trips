5d44008241135e85262fecd4625a320a16aa1bfc
Marcos Andre <marcosdev.me@gmail.com>
2022-05-13 13:21:58 -0300

    feat: creating dynamic pages

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,15 +1,16 @@
 import client from 'graphql/client'
-import { GET_PAGES } from 'graphql/querys'
+import { GET_PAGES, GET_PAGE_BY_SLUG } from 'graphql/querys'
 import { useRouter } from 'next/router'
+import { GetStaticProps } from 'next'
 
-import PageTemplate from 'templates/Pages'
+import PageTemplate, { PageTemplateProps } from 'templates/Pages'
 
-export default function AboutPage() {
+export default function Page({ heading, body }: PageTemplateProps) {
   const router = useRouter()
 
   if (router.isFallback) return null
 
-  return <PageTemplate />
+  return <PageTemplate heading={heading} body={body} />
 }
 
 export async function getStaticPaths() {
@@ -25,15 +26,20 @@ export async function getStaticPaths() {
   }
 }
 
-// export const getStaticProps = async () => {
-//   const { pages } = await client.request(GET_PAGES)
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { page } = await client.request(GET_PAGE_BY_SLUG, {
+    slug: `${params?.slug}`
+  })
 
-//   console.log(pages)
+  if (!page) return { notFound: true }
 
-//   return {
-//     props: {}
-//   }
-// }
+  return {
+    props: {
+      heading: page.heading,
+      body: page.body.html
+    }
+  }
+}
 
 // getStaticPaths => serve para gerar as urls em build time: /about
 // getStaticProps => serve para buscar dados da pagina (props) - build time
--- src/templates/Pages/index.tsx
+++ src/templates/Pages/index.tsx
@@ -3,24 +3,21 @@ import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline'
 
 import LinkWrapper from 'components/LinkWrapper'
 
-const PageTemplate = () => (
+export type PageTemplateProps = {
+  heading: string
+  body: string
+}
+
+const PageTemplate = ({ heading, body }: PageTemplateProps) => (
   <S.Content>
     <LinkWrapper href="/">
       <CloseOutline size={32} />
     </LinkWrapper>
 
-    <S.Heading>My Trips</S.Heading>
+    <S.Heading>{heading}</S.Heading>
 
     <S.Body>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque porta,
-        odio in tincidunt faucibus, lectus tortor molestie dui, at interdum diam
-        nulla sit amet arcu. Curabitur a sagittis sem. Mauris lacinia arcu et
-        nibh iaculis, vitae ornare arcu accumsan. Nunc non risus erat. Nullam
-        non diam luctus, interdum nibh vitae, ullamcorper neque. Vivamus non
-        ligula ut lorem pharetra finibus eu a tortor. Suspendisse eu porta
-        mauris, in imperdiet nunc.
-      </p>
+      <div dangerouslySetInnerHTML={{ __html: body }} />
     </S.Body>
   </S.Content>
 )
